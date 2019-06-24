739d0c077d9cb85910689d93c4c13619bc17ef5f
Dave Graff <daveagraff@gmail.com>
2019-06-23 23:50:01 -0400

    Made menu screen

--- Text Based Game/Program.cs
+++ Text Based Game/Program.cs
@@ -11,11 +11,33 @@ namespace Text_Based_Game {
 
     class Program {
         static void Main(string[] args) {
-            //Thread music = new Thread(playSong);
             Thread t = new Thread(() => playSong("Mario-Sheet-Music-Death-Sound.mid"));
             t.Start();
 
+            Console.WriteLine("Welcome to [Game Name!]");
+            Console.WriteLine("[S]tart\n[L]oad\n[E]xit");
+
+            char[] controlKeys = new char[] {'s', 'l', 'e'};
+
+            char inputChar;
+            do {
+                inputChar = Console.ReadKey().KeyChar;
+            } while (!controlKeys.Contains(inputChar));
+
+            switch (inputChar) {
+                case 'e':
+                case 's':
+                case 'l':
+                    Console.WriteLine("Unsupported :(");
+                    break;
+            }
+            
         }
+
+
+
+
+
 
         static void playSong(string fileName) {
             var midiFile = MidiFile.Read("../../Sounds/" + fileName);
