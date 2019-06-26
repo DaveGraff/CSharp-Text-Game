fe9768104c10721d46687eeb317accdc9c603746
mfoydl <31784414+mfoydl@users.noreply.github.com>
2019-06-25 22:04:01 -0400

    Created a simple beginning gameflow logic

--- Text Based Game/Program.cs
+++ Text Based Game/Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.Threading;
 using System.Linq;
-using System.Media;
 using System.Windows.Media;
 using System.Windows.Threading;
 
 namespace Text_Based_Game {
     class Program {
         static Thread music;
+
+        static State GameState;
+
+        //Constants for selection options in each location
+        static readonly string[] homeOptions = {"[R]est at the Inn","[V]isit the Shop", "[F]ight Monsters", "[E]xit without saving"};
+        static readonly string[] fightingOptions = { "[K]ill yourself", "[R]eturn Home" };
+        static readonly string[] shop = { "[E]xit the shop" };
+        static readonly string[] dead = { "You Died..." };
+
+        //Different States to describe the characters current location in the game
+        //Used by GameState variable
+        enum State { Exit, Dead, Home, Fighting, Shop }; 
+
         static void Main(string[] args) {
-            music = new Thread(() => playBackground("Button Press.wav"));
+            music = new Thread(() => PlayBackground("Menu.wav"));
             music.Start();
 
             Console.WriteLine("Welcome to [Game Name!]");
@@ -23,12 +35,13 @@ namespace Text_Based_Game {
                 inputChar = Console.ReadKey().KeyChar;
             } while (!controlKeys.Contains(inputChar));
 
+            Selection();
             switch (inputChar) {
                 case 'e':
-                    System.Environment.Exit(0);
+                    Environment.Exit(0);
                     break;
                 case 's':
-                    startGame();
+                    StartGame();
                     break;
                 case 'l':
                     Console.WriteLine("Unsupported :(");
@@ -36,24 +49,111 @@ namespace Text_Based_Game {
             }
         }
 
-        static void startGame() {
-            Thread test = new Thread(()=>playSong("Button Press.wav"));
-            test.Start();
-            Console.ReadKey();
+        static void StartGame() {
+            GameState = State.Home;
+            while (GameState != State.Exit) {
+                PrintOptions();
+                GameStep(Console.ReadKey().KeyChar);
+            }
         }
+
+        //Prints avaiable options based on GameState and Options lists
+        static void PrintOptions() {
+            string[] options;
+            switch (GameState) {
+                case State.Home:
+                    options = homeOptions;
+                    break;
+                case State.Fighting:
+                    options = fightingOptions;
+                    break;
+                case State.Shop:
+                    options = shop;
+                    break;
+                case State.Dead:
+                    options = dead;
+                    Dead();
+                    break;
+                default:
+                    options = new string[0];
+                    break;
+            }
+            Console.WriteLine();
+            foreach(string option in options) {
+                Console.WriteLine(option);
+            }
+
+        }
+
+        static void GameStep(char selection) {
+            Selection();
+            Console.WriteLine();
+            switch (GameState) {
+                case State.Home:
+                    switch (selection) {
+                        case 'r':
+                            Console.WriteLine("Taking a quick nap....");
+                            break;
+                        case 'v':
+                            GameState = State.Shop;
+                            break;
+                        case 'f':
+                            GameState = State.Fighting;
+                            break;
+                        case 'e':
+                            GameState = State.Exit;
+                            break;
+                            
+                    }
+                    break;
+                case State.Fighting:
+                    switch (selection) {
+                        case 'k':
+                            GameState = State.Dead;
+                            break;
+                        case 'r':
+                            GameState = State.Home;
+                            break;
+                    }
+                    break;
+                case State.Shop:
+                    switch (selection) {
+                        case 'e':
+                            GameState = State.Home;
+                            break;
+                    }
+                    break;
+            }
+
+        }
+
+        static void Dead() {
+            music.Abort();
+            Thread sound = new Thread(() => PlaySong("Mario-Sheet-Music-Death-Sound.mid"));
+            sound.Start();
+        }
+
 
         static void loadGame() {
 
         }
 
+        //Plays in game selection sound
+        static void Selection() {
+            Thread sound = new Thread(() => PlaySong("Button Press.wav"));
+            sound.Start();
+        }
 
-        static void playSong(string fileName) {
+        //Plays any general sound file 
+        static void PlaySong(string fileName) {
             var p1 = new MediaPlayer();
             fileName = AppContext.BaseDirectory + "../../Sounds/" + fileName;
             p1.Open(new Uri(@fileName));
             p1.Play();
         }
-        static void playBackground(string fileName) {
+
+        //Thread method for looping backgound Music
+        static void PlayBackground(string fileName) {
             Dispatcher.CurrentDispatcher.VerifyAccess();
 
             MediaPlayer backGroundMusic = new MediaPlayer();
