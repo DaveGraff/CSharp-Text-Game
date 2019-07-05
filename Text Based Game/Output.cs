954bd0220ddd92625d18983bcbb3abc1dddca97d
Dave Graff <daveagraff@gmail.com>
2019-07-04 23:58:15 -0400

    Added ability to parse metadata commands. Added full list of game states to be filled in soon.

--- Text Based Game/Output.cs
+++ Text Based Game/Output.cs
@@ -8,40 +8,35 @@ using System.Threading.Tasks;
 namespace Text_Based_Game {
     class Output {
         private static Dictionary<string, string> states = new Dictionary<string, string>() {
-            {"menu", "Welcome to Adventures United!\n[S]tart\n[L]oad\n[E]xit" },
-            {"home", "[R]est at the Inn\n[V]isit the Shop\n[F]ight Monsters\n[E]xit without saving" },
+            {"menu", "Welcome to Adventures United! \n[S]tart \n[L]oad \n[E]xit" },
+            {"home", "[R]est at the Inn \n[V]isit the Shop \n[G]o Adventuring \n[E]xit without saving" },
             {"dead", "You died..." },
             {"shop", "[E]xit the shop" },
-            {"adventure", "[F]ight monster\n[R]eturn" },
-            {"playerturn", "[A]ttack\n[U]se Item\n[R]un away" }
+            {"adventure", "[F]ight monster \n[R]eturn" },
+            {"playerturn", "[A]ttack\n[U]se Item\n[R]un away" },
+            {"load", "" }//Use metadata to see how many saves are left
         };
 
-        private static string metaData { get; set; }
+        public static string MetaData { get; set; }
 
 
         public static Char displayState(string state) {
+            //Get acceptable commands as everything in the state between square brackets
             string regex = ("\\[.\\]");
-            MatchCollection matches = Regex.Matches(states[state], regex);
+            MatchCollection matches = Regex.Matches(states[state] + MetaData, regex);
             Char[] options = matches.Cast<Match>().Select(m => m.Value[1]).ToArray();
 
             Char keyVal = '\n';
             while (!options.Contains(keyVal)) {
                 Console.Clear();
-                Console.WriteLine(states[state]);
-                printMeta();
+                Console.WriteLine(states[state] + MetaData);
                 keyVal = Char.ToUpper(Console.ReadKey().KeyChar);
                 Jukebox.PlaySong("Button Press.wav");
             }
 
-            metaData = "";
+            MetaData = "";
 
             return keyVal;
-        }
-
-        private static void printMeta() {
-            if (metaData.Length != 0) {
-                Console.WriteLine("\n\n" + metaData);
-            }
         }
     }
 }
--- Text Based Game/Program.cs
+++ Text Based Game/Program.cs
@@ -8,53 +8,99 @@ using System.Windows.Threading;
 
 namespace Text_Based_Game {
     class Program {
-        private static Jukebox music = new Jukebox("../../Sounds/");
+        private static readonly Jukebox music = new Jukebox("../../Sounds/");
 
         static void Main(string[] args) {
             music.PlayBackground("Menu.wav");
-            menuState();
+            MenuState();
         }
 
-        static void menuState() {
+        static void MenuState() {
             switch (Output.displayState("menu")) {
                 case 'S':
-                    homeState();
+                    HomeState();
                     break;
                 case 'L':
+                    LoadState();
                     break;
                 case 'E':
-                    exit();
+                    Exit();
                     break;
             }
         }
 
-        static void homeState() {
+        static void HomeState() {
             switch (Output.displayState("home")) {
                 case 'R':
-                    for(int i = 0; i < 2; i++) {
+                    for (int i = 0; i < 2; i++) {
                         Console.Clear();
-                        Console.WriteLine("Z");
+                        Console.Write("Z ");
                         Thread.Sleep(300);
-                        Console.WriteLine("Z");
+                        Console.Write("Z ");
                         Thread.Sleep(300);
-                        Console.WriteLine("Z...");
+                        Console.Write("Z...");
                         Thread.Sleep(300);
                     }
-                    homeState();
+                    HomeState();
                     break;
                 case 'V':
+                    ShopState();
                     break;
-                case 'F':
+                case 'G':
+                    AdventureState();
                     break;
                 case 'E':
-                    menuState();
+                    MenuState();
                     break;
             }
         }
 
-        static void exit() {
+        static void LoadState() {
+            //TODO: Logic for load game
+            Output.MetaData = "[R]eturn";
+            switch (Output.displayState("load")) {
+                case 'R':
+                    MenuState();
+                    break;
+            }
+        }
+
+        static void ShopState() {
+            //TODO: Logic for buying items
+            switch (Output.displayState("shop")) {
+                case 'E':
+                    HomeState();
+                    break;
+            }
+        }
+
+        static void AdventureState() {
+            switch (Output.displayState("adventure")) {
+                case 'F':
+                    FightState();
+                    break;
+                case 'R':
+                    HomeState();
+                    break;
+            }
+
+        }
+        
+        static void FightState() {
+            switch (Output.displayState("playerturn")) {
+                case 'A':
+                    break;
+                case 'U':
+                    break;
+                case 'R':
+                    AdventureState();
+                    break;
+            }
+        }
+
+        static void Exit() {
             music.Stop();
             Environment.Exit(0);
-        }       
+        }
     }
 }
